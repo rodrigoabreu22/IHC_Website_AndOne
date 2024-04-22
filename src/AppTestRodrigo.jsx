cfd9031f9bfb862578f1e507149d8506b47e8f0c
rodrigoabreu22 <rgana22svb@gmail.com>
2024-04-22 19:15:53 +0100

    carrossel com bastante progresso, falta linkar e fazer uns ajustes

---
+++ src/AppTestRodrigo.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HomeCarousel from './Components/HomeCarousel'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HomeCarousel/>
+    </>
+  )
+}
+
+export default App
