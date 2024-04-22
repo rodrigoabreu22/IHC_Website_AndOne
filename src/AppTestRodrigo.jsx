5e687fcacb3300ad12603289212b6a8885bc064c
rodrigoabreu22 <rgana22svb@gmail.com>
2024-04-22 19:34:29 +0100

    teste da pagina inicial

--- src/AppTestRodrigo.jsx
+++ src/AppTestRodrigo.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomeCarousel from './Components/HomeCarousel'
+import HomeCarousel from './components/HomeCarousel'
+import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MyNavbar/>
       <HomeCarousel/>
+      <MyFooter/>
     </>
   )
 }
