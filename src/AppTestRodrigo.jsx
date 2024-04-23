a95985902b48c48aa28497f11d9f68465fe509dd
rodrigoabreu22 <rgana22svb@gmail.com>
2024-04-23 22:50:53 +0100

    product page the beginning

--- src/AppTestRodrigo.jsx
+++ src/AppTestRodrigo.jsx
@@ -5,14 +5,18 @@ import './App.css'
 import HomeCarousel from './components/HomeCarousel'
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
+import Product from './components/Product'
+import ProductList from './data/Products.json'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const product1 = ProductList.products[0]
+
   return (
     <>
       <MyNavbar/>
-      <HomeCarousel/>
+      <Product product={product1} />
       <MyFooter/>
     </>
   )
