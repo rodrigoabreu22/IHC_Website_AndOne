9d4478a4d5d8b7216533340525d4720d965264da
odraude23 <eduardolplopes@ua.pt>
2024-04-26 14:55:31 +0100

    detalhes dos pedidos

---
+++ src/Components/DetalhesPedido.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Container, Row } from 'react-bootstrap';
+import ProductList from '../data/Products.json';
+
+// alterar para utilizar o array passado por props
+
+function DetalhesPedido(props) {
+    return (
+        <Container fluid className="d-flex justify-content-end p-3"> 
+            <div style={{ width: '25%', backgroundColor: 'gray', color: 'white', padding: '1rem' }}>
+                <Row>
+                    <h3>Detalhes do Pedido</h3>
+                </Row>
+                <Row>
+                    <ul style={{ paddingLeft: 2 }}>
+                        {ProductList && ProductList.products.map(product => (
+                            <li key={product.id}>
+                                <Row  className="d-flex p-1">
+                                    <Col style={{ maxWidth: 100 }}>
+                                        <img src={product.image_link[0]} alt={product.name} style={{ width: '60px' }} />
+                                    </Col>
+                                    <Col>
+                                        <p>{product.name}</p>
+                                        <p>{product.price}</p>
+                                    </Col>
+                                </Row>
+                            </li>
+                        ))}
+                    </ul>
+                </Row>
+            </div>
+        </Container>
+    );
+}
+
+export default DetalhesPedido;
