f95a476d9b1977e5c9e9ca0d1977befed342ff80
rodrigoabreu22 <rgana22svb@gmail.com>
2024-04-26 19:28:23 +0100

    progresso nos favoritos

--- src/pages/FavoritosPage.jsx
+++ src/pages/FavoritosPage.jsx
@@ -2,38 +2,21 @@ import React, { useState, useEffect } from "react";
 import MyNavbar from "../components/MyNavbar";
 import MyFooter from "../components/MyFooter";
 import ProductCard from '../components/ProductCard';
+import ProductList from '../data/Products.json';
 import '../AppTestRicardo.css'; // Import the CSS file
 
 const FavoritosPage = () => {
-    const [favoriteProducts, setFavoriteProducts] = useState([]);
-
-    useEffect(() => {
-        // Retrieve favorite product IDs from local storage
-        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-        
-        // Fetch corresponding products based on IDs
-        // Assuming you have a function to fetch products by ID
-        const fetchFavoriteProducts = async () => {
-            const fetchedProducts = await Promise.all(
-                favorites.map(async productId => {
-                    // Assuming getProductById is a function to fetch product by ID
-                    const product = await getProductById(productId);
-                    return product;
-                })
-            );
-            setFavoriteProducts(fetchedProducts);
-        };
-
-        fetchFavoriteProducts();
-    }, []);
+    const [ favoriteProds, setFavoriteProducts ] = useState(localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : []);
+    
+    const favoriteProd = ProductList.products.filter(product => favoriteProds.includes(product.id));
 
     return (
         <div>
             <MyNavbar activeID={7} />
             <h1>Favorites</h1>
             <div className="favorite-products">
-                {favoriteProducts.map(product => (
-                    <ProductCard key={product.id} product={product} />
+                {favoriteProd.map(product => (
+                    <ProductCard product={product} favorites={favoriteProds} setFavorites={setFavoriteProducts}  />
                 ))}
             </div>
             <MyFooter />
