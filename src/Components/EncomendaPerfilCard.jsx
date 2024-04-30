bbd1f5920d9d3a67b302d9c6b2c725e469794a86
ricardo.alexandre.antunes@ua.pt <ricardo.alexandre.antunes@ua.pt>
2024-04-30 19:09:45 +0100

    a

--- src/Components/EncomendaPerfilCard.jsx
+++ src/Components/EncomendaPerfilCard.jsx
@@ -65,11 +65,11 @@ function EncomendaPerfilCard({item}) {
             <Card className="product-card">
                 <Card.Body>
                     <Card.Text className="product-cart-info">
-                        <div className="order-id"><strong>ID: </strong>{curItem["id"]}</div>
-                        <div className="order-date"><strong>Data: </strong>{curItem["data"]}</div>
-                        <div className="order-price"><strong>Preço: </strong>{curItem["total"].toFixed(2)}€</div>
-                        <button className='show-more-details-button' variant="primary" onClick={handleShowModal}>Ver detalhes</button>
-                        <button className='show-products-button' variant="primary" onClick={handleShowProductsModal}>Ver produtos</button>
+                        <div className="order-id" style={{width: '20%'}}><strong>ID: </strong>{curItem["id"]}</div>
+                        <div className="order-date" style={{width: '20%'}}><strong>Data: </strong>{curItem["data"]}</div>
+                        <div className="order-price" style={{width: '20%'}}><strong>Preço: </strong>{curItem["total"].toFixed(2)}€</div>
+                        <button className='show-more-details-button' variant="primary" onClick={handleShowModal} style={{width: '20%'}}>Ver detalhes</button>
+                        <button className='show-products-button' variant="primary" onClick={handleShowProductsModal} style={{width: '20%'}}>Ver produtos</button>
                     </Card.Text>
                 </Card.Body>
             </Card>
